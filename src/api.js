cc6754c391ac80bc4fe554da57a55abb31db8794
Tanmay G Shetty <tanmay.121cr7@gmail.com>
2025-05-08 09:18:02 +0530

    Base url added

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api" // update if deployed
+  baseURL: "https://leaderboard-backend-neon.vercel.app/" // update if deployed
 });
